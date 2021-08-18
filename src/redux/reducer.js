363a139054f6daea98642bfe1582d0d55199dc80
Raj Kumar <interplaytheheads@gmail.com>
2021-08-19 00:54:29 +0300

    reducer updated for 5 increment

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -9,10 +9,23 @@ const reducer = (state = initialState, action) => {
                 ...state,
                 counter: state.counter + 1,
             }
+
         case 'DECREMENT':
             return{
                 ...state,
                 counter: state.counter - 1,
+            }
+
+        case 'INCREMENT_WITH_PAYLOAD':
+            return{
+                ...state,
+                counter: state.counter + action.payload
+            }
+        
+        case 'DECREMENT_WITH_PAYLOAD':
+            return{
+                ...state,
+                counter: state.counter - action.payload
             }
         default: 
         return state
